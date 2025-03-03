2ce416551b032ac1f03682b5f3438ba8aefee62b
Jeremy Elliott <jjelliot@uvm.edu>
2025-03-03 17:20:50 -0500

    Renames and prettier plots

--- Code/Utility/toolbox.py
+++ Code/Utility/toolbox.py
@@ -1,7 +1,8 @@
-import git
+# import git
 import ast 
 import bs4 as BeautifulSoup
 import requests
+import os
 
 from datetime import datetime
 from functools import wraps
@@ -14,9 +15,24 @@ def printline():
 
 ## file management
 def find_repo_root():
+    import git
     repo = git.Repo(search_parent_directories=True)
     root = repo.git.rev_parse("--show-toplevel")
     return root.replace("\\", "/")
+
+def nogit_find_repo_root(startpath):
+    current_path = os.path.abspath(startpath) # Path started on
+    while True:
+        if os.path.isdir(os.path.join(current_path, '.git')) or os.path.isfile(os.path.join(current_path, 'README.md')): # If on git path, return it
+            return current_path
+        
+        parent_path = os.path.dirname(current_path)
+
+        if parent_path == current_path: # If current path is parent path, stop 
+            break
+        current_path = parent_path # Set current path to parent path, to check if git path again
+    return None # If an issue arises 
+# root = find_repo_root(os.getcwd()) # Good to set as a global variable 
 
 
 ## data structure management (for dicts inside of dataframes, etc)
